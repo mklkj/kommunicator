bd8346121baebeb198af46b0799b04e999cc8f81
Mikołaj Pich <m.pich@outlook.com>
2024-03-17 20:21:22 +0100

    Fix common tests

--- composeApp/src/commonTest/kotlin/io/github/mklkj/kommunicator/data/repository/MessagesRepositoryTest.kt
+++ composeApp/src/commonTest/kotlin/io/github/mklkj/kommunicator/data/repository/MessagesRepositoryTest.kt
@@ -1,15 +1,14 @@
 package io.github.mklkj.kommunicator.data.repository
 
 import io.github.mklkj.kommunicator.data.api.service.MessagesService
-import io.github.mklkj.kommunicator.data.db.Database
 import io.github.mklkj.kommunicator.data.models.Chat
 import io.github.mklkj.kommunicator.data.models.ChatCreateRequest
-import io.github.mklkj.kommunicator.data.models.ChatDetails
-import io.github.mklkj.kommunicator.data.models.MessageRequest
+import io.github.mklkj.kommunicator.data.models.ChatCreateResponse
+import io.github.mklkj.kommunicator.data.models.Message
+import io.github.mklkj.kommunicator.data.models.MessageEvent
 import kotlinx.coroutines.test.runTest
 import kotlinx.uuid.UUID
 import kotlin.test.Test
-import kotlin.test.assertEquals
 
 class MessagesRepositoryTest {
 
@@ -17,9 +16,12 @@ class MessagesRepositoryTest {
     fun `test messages service`() = runTest {
         val service = object : MessagesService {
             override suspend fun getChats(): List<Chat> = emptyList()
-            override suspend fun createChat(body: ChatCreateRequest) = Unit
-            override suspend fun getChat(id: UUID) = ChatDetails("", emptyList())
-            override suspend fun sendMessage(chatId: UUID, message: MessageRequest) = Unit
+            override suspend fun getChat(id: UUID) = Chat(UUID(), "", emptyList(), null, null)
+            override suspend fun getMessages(chatId: UUID): List<Message> = emptyList()
+            override suspend fun sendMessage(chatId: UUID, message: MessageEvent) = Unit
+            override suspend fun createChat(body: ChatCreateRequest): ChatCreateResponse {
+                TODO("Not yet implemented")
+            }
         }
 //        val repo = MessagesRepository(service)
 //
